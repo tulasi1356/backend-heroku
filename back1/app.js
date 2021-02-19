ab026184d02b3373a322193b1262c0e6dd58156f
balu balaji <sankubhavyasri@gmail.com>
2021-02-19 19:20:11 +0530

    pusH

--- back1/app.js
+++ back1/app.js
@@ -11,7 +11,7 @@ const PORT = process.env.PORT || 5000;
 const path=require('path')
 
 var MongoClient = require('mongodb').MongoClient;
-var url = "mongodb://localhost:27017/";
+// var url = "mongodb://localhost:27017/";
 const bodyparser=require('body-parser') 
 connectDB()
 app.use(express.json());
